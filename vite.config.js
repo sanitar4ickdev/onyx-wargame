7cea6ee2a4207d3707ac238105752cdb6e9c3708
sanitar4ickdev <tsanitar2007@gmail.com>
2026-06-02 22:54:38 +0200

    fix vite config typo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-vite'
+   import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-plugins: [react()],
-base: '/onyx-wargame/',
-})
+   export default defineConfig({
+     plugins: [react()],
+     base: '/onyx-wargame/',
+   })
